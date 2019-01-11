707cbb8bf4d1ccc28e7e56ec41b60399c259c908
xuehua <xuehua@gmail.com>
2019-01-11 08:42:31 -0800

    Create some python codes for EOPI

---
+++ EOPI/chapter14/rebuild_bst_from_preoder.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 10 21:34:47 2019
+
+@author: windr
+"""
+
+from userlib import BinaryTree 
+
+BinaryTree.init(0)
+
+print (BinaryTree.val)
---
+++ EOPI/lib/userlib/BinaryTree.py
@@ -0,0 +1,8 @@
+
+class BinaryTreeNode:
+    def __init_(self, x):
+        self.val = x
+        self.left = None
+        self.right= None
+    
+    
